d2aaa4b9fc4850f19781e444ad03a499fa3061bf
Michael Mauderer <michael@michaelmauderer.com>
2019-03-04 22:18:50 +0000

    Add implementation of discrete Bayes filter prediction.

--- src/common.rs
+++ src/common.rs
@@ -1,38 +1,31 @@
 /*!
 Provides utility functions used in other parts of the library.
 */
+use std::num::Wrapping;
+
 use num_traits::Float;
 
 /// Determines how the convolution is computed. This mostly effects behaviour at the boundaries.
-pub(crate) enum ConvolutionMode {
-    /// Returns the convolution at each point of overlap.
-    Full,
-    /// Returns the convolution at each point of the signal.
-    Same,
-    /// Returns the convolution at each point where the signals overlap completely.
-    Valid,
+pub(crate) enum ConvolutionMode<F> {
+    /// Returns the convolution at each point of overlap, assuming the signals wrap around.
+    Wrap,
+    /// Returns the convolution at each point of overlap, assuming the signals
+    /// are extended by the given value around.
+    Extended(F),
 }
 
 /// Compute the discrete convolution of the two slices.
 /// This might be slow, as this function is not optimised in any way.
-pub(crate) fn convolve<F: Float>(a: &[F], b: &[F], mode: ConvolutionMode) -> Vec<F> {
-    let (a, b) = if a.len() < b.len() {
-        (b, a)
-    } else {
-        (a, b)
-    };
+pub(crate) fn convolve<F: Float>(a: &[F], b: &[F], mode: ConvolutionMode<F>) -> Vec<F> {
+    let (a, b) = if a.len() < b.len() { (b, a) } else { (a, b) };
 
     match mode {
-        ConvolutionMode::Full => convolve_full(a, b),
-        ConvolutionMode::Same => convolve_same(a, b),
-        ConvolutionMode::Valid => convolve_valid(a, b),
+        ConvolutionMode::Wrap => convolve_wrap(a, b),
+        ConvolutionMode::Extended(c) => convolve_extended(a, b, c),
     }
 }
 
-
-/// Compute convolution between signal and window for every index in window.
-/// Result will have same length as signal.
-fn convolve_same<F: Float>(signal: &[F], window: &[F]) -> Vec<F> {
+fn convolve_extended<F: Float>(signal: &[F], window: &[F], c: F) -> Vec<F> {
     let m = signal.len() as i64;
     let n = window.len() as i64;
     debug_assert!(m >= n);
@@ -42,14 +35,14 @@ fn convolve_same<F: Float>(signal: &[F], window: &[F]) -> Vec<F> {
         let mut x = F::zero();
         for j in 0..n {
             let s_ij = {
-                let ix = i - j + n / 2;
+                let ix = i + j - n / 2;
                 if ix < 0 {
-                    F::zero()
+                    c
                 } else {
-                    *signal.get(ix as usize).unwrap_or(&F::zero())
+                    *signal.get(ix as usize).unwrap_or(&c)
                 }
             };
-            let &w_ij = window.get(j as usize).unwrap_or(&F::zero());
+            let w_ij = *window.get(j as usize).unwrap_or(&c);
             x = x + s_ij * w_ij;
         }
         result.push(x)
@@ -57,65 +50,57 @@ fn convolve_same<F: Float>(signal: &[F], window: &[F]) -> Vec<F> {
     result
 }
 
-
-fn convolve_valid<F: Float>(signal: &[F], window: &[F]) -> Vec<F> {
+fn convolve_wrap<F: Float>(signal: &[F], window: &[F]) -> Vec<F> {
     let m = signal.len() as i64;
     let n = window.len() as i64;
     debug_assert!(m >= n);
 
-    let mut result = Vec::default();
+    let mut result = Vec::with_capacity(m as usize);
+    result.resize_with(m as usize, F::zero);
+
     for i in 0..m {
-        let mut x = F::zero();
-        let mut valid = true;
         for j in 0..n {
             let s_ij = {
-                let ix = i - j + n / 2;
-                if ix < 0 {
-                    None
-                } else {
-                    signal.get(ix as usize)
-                }
+                let ix = (i + j - (n / 2)) % m;
+                let ix = if ix < 0 { ix + m } else { ix };
+                signal[ix as usize]
             };
-            let w_ij = window.get(j as usize);
-
-            if s_ij.is_none() || w_ij.is_none() {
-                valid = false;
-                break;
-            }
-
-            x = x + (*s_ij.unwrap()) * (*w_ij.unwrap());
-        }
-        if valid {
-            result.push(x)
+            let w_ij = window[j as usize];
+            result[i as usize] = result[i as usize] + (s_ij * w_ij);
         }
     }
     result
 }
 
-
-fn convolve_full<F: Float>(signal: &[F], window: &[F]) -> Vec<F> {
-    let m = signal.len() as i64;
-    let n = window.len() as i64;
-    debug_assert!(m >= n);
-
-    let mut result = Vec::default();
-    for i in (-n / 2)..(m + n / 2) {
-        let mut x = F::zero();
-        for j in 0..n {
-            let s_ij = {
-                let ix = i - j + n / 2;
-                if ix < 0 {
-                    F::zero()
-                } else {
-                    *signal.get(ix as usize).unwrap_or(&F::zero())
-                }
-            };
-            let w_ij = *window.get(j as usize).unwrap_or(&F::zero());
-            x = x + s_ij * w_ij;
-        }
-        result.push(x)
+fn roll<T: Copy>(a: &[T], shift: i64) -> Vec<T> {
+    let mut out = Vec::with_capacity(a.len());
+    for i in 0..a.len() as i64 {
+        let ix = (Wrapping(i) - Wrapping(shift)).0 % (a.len() as i64 - 1);
+        let ix = if ix < 0 { ix + a.len() as i64 } else { ix };
+        out.push(a[ix as usize])
     }
-    result
+    out
+}
+
+fn shift_extend<T: Copy>(a: &[T], shift: i64, value: T) -> Vec<T> {
+    let mut out: Vec<T> = Vec::with_capacity(a.len());
+    for i in 0..a.len() as i64 {
+        let ix = i - shift;
+        out.push(*a.get(ix as usize).unwrap_or(&value))
+    }
+    out
+}
+
+pub(crate) enum ShiftMode<F> {
+    Wrap,
+    Extend(F),
+}
+
+pub(crate) fn shift<T: Copy>(a: &[T], shift: i64, mode: ShiftMode<T>) -> Vec<T> {
+    match mode {
+        ShiftMode::Wrap => roll(a, shift),
+        ShiftMode::Extend(c) => shift_extend(a, shift, c),
+    }
 }
 
 #[cfg(test)]
@@ -125,41 +110,66 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_convolve_same() {
+    fn test_convolve_extended() {
         let a: [f64; 3] = [1.0, 2.0, 3.0];
         let b: [f64; 3] = [0.0, 1.0, 0.5];
 
-        let c = convolve(&a, &b, ConvolutionMode::Same);
+        let c = convolve(&a, &b, ConvolutionMode::Extended(10.0));
 
         debug_assert_eq!(3, c.len());
-        assert_approx_eq!(1.0, c[0]);
-        assert_approx_eq!(2.5, c[1]);
-        assert_approx_eq!(4.0, c[2]);
+        assert_approx_eq!(2.0, c[0]);
+        assert_approx_eq!(3.5, c[1]);
+        assert_approx_eq!(8.0, c[2]);
     }
 
     #[test]
-    fn test_convolve_valid() {
+    fn test_convolve_wrap() {
         let a: [f64; 3] = [1.0, 2.0, 3.0];
         let b: [f64; 3] = [0.0, 1.0, 0.5];
 
-        let c = convolve(&a, &b, ConvolutionMode::Valid);
+        let c = convolve_wrap(&a, &b);
 
-        debug_assert_eq!(1, c.len());
-        assert_approx_eq!(2.5, c[0]);
+        debug_assert_eq!(3, c.len());
+        dbg!(&c);
+        assert_approx_eq!(2.0, c[0]);
+        assert_approx_eq!(3.5, c[1]);
+        assert_approx_eq!(3.5, c[2]);
     }
 
     #[test]
-    fn test_convolve_full() {
-        let a: [f64; 3] = [1.0, 2.0, 3.0];
-        let b: [f64; 3] = [0.0, 1.0, 0.5];
+    fn test_convolve_wrap2() {
+        let a = vec![3.0, 1.0, 2.0, 3.0, 2.0, 1.0, 1.0];
+        let b = vec![0.0, 1.0, 0.5];
 
-        let c = convolve(&a, &b, ConvolutionMode::Full);
+        let c = convolve_wrap(&a, &b);
 
-        debug_assert_eq!(5, c.len());
-        assert_approx_eq!(0.0, c[0]);
-        assert_approx_eq!(1.0, c[1]);
-        assert_approx_eq!(2.5, c[2]);
+        debug_assert_eq!(7, c.len());
+        dbg!(&c);
+        assert_approx_eq!(3.5, c[0]);
+        assert_approx_eq!(2.0, c[1]);
+        assert_approx_eq!(3.5, c[2]);
         assert_approx_eq!(4.0, c[3]);
-        assert_approx_eq!(1.5, c[4]);
+        assert_approx_eq!(2.5, c[4]);
+        assert_approx_eq!(1.5, c[5]);
+        assert_approx_eq!(2.5, c[6]);
+    }
+
+    #[test]
+    fn test_roll() {
+        let a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        let c = roll(&a, 2);
+
+        debug_assert_eq!(10, c.len());
+        debug_assert_eq!(8, c[0]);
+        debug_assert_eq!(9, c[1]);
+        debug_assert_eq!(0, c[2]);
+        debug_assert_eq!(1, c[3]);
+        debug_assert_eq!(2, c[4]);
+        debug_assert_eq!(3, c[5]);
+        debug_assert_eq!(4, c[6]);
+        debug_assert_eq!(5, c[7]);
+        debug_assert_eq!(6, c[8]);
+        debug_assert_eq!(7, c[9]);
     }
 }
--- src/discrete_bayes.rs
+++ src/discrete_bayes.rs
@@ -3,6 +3,11 @@ Provides implementations of and related to Discrete Bayes filtering.
 */
 use num_traits::Float;
 
+use crate::common::convolve;
+use crate::common::shift;
+use crate::common::ConvolutionMode;
+use crate::common::ShiftMode;
+
 /// Normalize distribution `pdf` in-place so it sums to 1.0.
 ///
 /// # Example
@@ -25,7 +30,6 @@ pub fn normalize<F: Float>(pdf: &mut [F]) {
     pdf.iter_mut().for_each(|f| *f = *f / sum);
 }
 
-
 /// Computes the posterior of a discrete random variable given a
 /// discrete likelihood and prior. In a typical application the likelihood
 /// will be the likelihood of a measurement matching your current environment,
@@ -33,9 +37,13 @@ pub fn normalize<F: Float>(pdf: &mut [F]) {
 ///
 pub fn update<F: Float>(likelihood: &[F], prior: &[F]) -> Result<Vec<F>, ()> {
     if likelihood.len() != prior.len() {
-        return Err(())
+        return Err(());
     }
-    let mut posterior: Vec<F> = likelihood.iter().zip(prior.iter()).map(|(&l, &p)| l * p).collect();
+    let mut posterior: Vec<F> = likelihood
+        .iter()
+        .zip(prior.iter())
+        .map(|(&l, &p)| l * p)
+        .collect();
     normalize(&mut posterior);
     Ok(posterior)
 }
@@ -48,8 +56,92 @@ pub enum EdgeHandling<F> {
     Wrap,
 }
 
-
 /// Performs the discrete Bayes filter prediction step, generating the prior.
 pub fn predict<F: Float>(pdf: &[F], offset: i64, kernel: &[F], mode: EdgeHandling<F>) -> Vec<F> {
-    unimplemented!()
+    match mode {
+        EdgeHandling::Constant(c) => convolve(
+            &shift(pdf, offset, ShiftMode::Extend(c)),
+            kernel,
+            ConvolutionMode::Extended(c),
+        ),
+        EdgeHandling::Wrap => convolve(
+            &shift(pdf, offset, ShiftMode::Wrap),
+            kernel,
+            ConvolutionMode::Wrap,
+        ),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use assert_approx_eq::assert_approx_eq;
+
+    use super::*;
+
+    #[test]
+    fn test_prediction_wrap_kernel_3() {
+        let pdf = {
+            let mut pdf = [1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+            normalize(&mut pdf);
+            pdf
+        };
+
+        let kernel = [0.5, 0.5, 0.5, 0.5];
+
+        let result = predict(&pdf, -1, &kernel, EdgeHandling::Wrap);
+        let reference = [0.5, 0.0, 0.0, 0.0, 0.5, 0.5, 0.5];
+        dbg!(&result);
+        dbg!(&reference);
+
+        debug_assert_eq!(reference.len(), result.len());
+        for i in 0..reference.len() {
+            assert_approx_eq!(reference[i], result[i]);
+        }
+    }
+
+    #[test]
+    fn test_prediction_wrap_kernel_4() {
+        let pdf = {
+            let mut pdf = [0.0, 1.0, 2.0, 4.0, 8.0, 16.0, 8.0];
+            normalize(&mut pdf);
+            pdf
+        };
+
+        let kernel = [0.5, 0.5, 0.5, 0.5];
+
+        let result = predict(&pdf, 3, &kernel, EdgeHandling::Wrap);
+        let reference = [
+            0.29487179, 0.17948718, 0.08333333, 0.05128205, 0.05448718, 0.11217949, 0.22435897,
+        ];
+        dbg!(&result);
+        dbg!(&reference);
+
+        debug_assert_eq!(reference.len(), result.len());
+        for i in 0..reference.len() {
+            assert_approx_eq!(reference[i], result[i]);
+        }
+    }
+
+    #[test]
+    fn test_prediction_constant_kernel_4() {
+        let pdf = {
+            let mut pdf = [0.0, 1.0, 2.0, 4.0, 8.0, 16.0, 8.0];
+            normalize(&mut pdf);
+            pdf
+        };
+
+        let kernel = [0.5, 0.5, 0.5, 0.5];
+
+        let result = predict(&pdf, 3, &kernel, EdgeHandling::Constant(10.0));
+        let reference = [
+            10.0, 7.5, 2.50641026, 1.27564103, 0.05448718, 2.56089744, 7.51923077,
+        ];
+        dbg!(&result);
+        dbg!(&reference);
+
+        debug_assert_eq!(reference.len(), result.len());
+        for i in 0..reference.len() {
+            assert_approx_eq!(reference[i], result[i]);
+        }
+    }
 }
